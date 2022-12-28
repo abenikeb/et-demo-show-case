988a560452812a75724f49bcca954bb9c4c1dc83
ABENEZER KEBEDE <aWX1210953@china.huawei.com>
2022-12-28 11:56:59 -0800

    adding

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:8084/et-demo-php";
 
-const baseUrl = "http://et-demo-php.rf.gd/et-demo-php";
+const baseUrl = "https://et-web-php.000webhostapp.com/";
 
 const merchantAppId = "850259476582401";
